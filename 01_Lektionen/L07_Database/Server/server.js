cb895159b05c9322cd03841541182e79c13f56fd
ayaan99 <64155100+ayaan99@users.noreply.github.com>
2020-09-19 16:42:53 +0200

    neue Ordnerstruktur

--- 01_Lektionen/L07_Database/Server/server.js
+++ 01_Lektionen/L07_Database/Server/server.js
@@ -3,15 +3,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.L07_CocktailBar = void 0;
 const Http = require("http");
 const Url = require("url");
+const Mongo = require("mongodb");
 var L07_CocktailBar;
 (function (L07_CocktailBar) {
-    let server = Http.createServer();
+    let orders;
     let port = process.env.PORT;
     if (port == undefined)
         port = 5001;
-    console.log("Server starting on port:" + port);
-    server.listen(port);
-    server.addListener("request", handleRequest);
+    let databaseUrl = "mongo 'mongodb+srv://eia2.bg79w.mongodb.net/<dbname>' --username Nutzer1";
+    startServer(port);
+    connectToDatabase(databaseUrl);
+    function startServer(_port) {
+        let server = Http.createServer();
+        console.log("Server starting on port:" + _port);
+        server.listen(_port);
+        server.addListener("request", handleRequest);
+    }
+    async function connectToDatabase(_url) {
+        let options = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        orders = mongoClient.db("Cocktailbar").collection("Orders");
+        console.log("Database connection", orders != undefined);
+    }
     function handleRequest(_request, _response) {
         console.log("whats up?");
         _response.setHeader("content-type", "text/html; charset=utf-8");
@@ -23,9 +37,14 @@ var L07_CocktailBar;
             }
             let jsonString = JSON.stringify(url.query);
             _response.write(jsonString);
+            storeOrder(url.query);
         }
         _response.write("this is my response");
         _response.end();
     }
+    function storeOrder(_order) {
+        orders.insert(_order);
+    }
 })(L07_CocktailBar = exports.L07_CocktailBar || (exports.L07_CocktailBar = {}));
+// mongo "mongodb+srv://eia2.bg79w.mongodb.net/<dbname>" --username Nutzer1
 //# sourceMappingURL=server.js.map
